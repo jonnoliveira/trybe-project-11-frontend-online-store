a0035b3d15acd1cf31279301387ec5254f6779eb
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-09 19:24:05 -0300

    Requisito 3 - completo

--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import ButtonCart from '../components/ButtonCart';
 
 export default class ProductsList extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-        <input type="text" />
+      <div>
+        <div data-testid="home-initial-message">
+          <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          <input type="text" />
+        </div>
+        <ButtonCart />
       </div>
     );
   }
