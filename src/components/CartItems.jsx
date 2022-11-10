289bfee8124a180fcdec14787afe5c28c4f16020
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-10 20:38:02 -0300

    Requisito 7 - Parcial/Atributos

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './CartItem.css';
 
 export default class CartItems extends Component {
   render() {
     const { item } = this.props;
-    const { title, thumbnail, price } = item;
+    const { title, thumbnail, price, id } = item;
     return (
       <div data-testid="product" className="products">
-        <img src={ thumbnail } alt={ title } />
-        <h3>
-          { title }
-        </h3>
-        <h4>
-          { price }
-        </h4>
+        <Link
+          to={ `/productdetails/${id}` }
+          data-testid="product-detail-link"
+        >
+          <img src={ thumbnail } alt={ title } />
+          <h3>
+            { title }
+          </h3>
+          <h4>
+            { price }
+          </h4>
+        </Link>
       </div>
     );
   }
@@ -25,5 +31,6 @@ CartItems.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
