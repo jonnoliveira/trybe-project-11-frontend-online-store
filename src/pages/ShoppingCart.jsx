0978fe5327f72d36f90027a532cff67af1a37cc2
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-11 19:01:03 -0300

    Requisito 10 - Incompleto

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 export default class ShoppingCart extends Component {
+  // state = {
+  //   quantity: 0,
+  // };
+
   recoverLocalStorage = () => {
     const products = JSON.parse(localStorage.getItem('savedItems'));
     return products;
   };
 
+  incrementProduct = ({ target }) => {
+    const element = target.parentNode.firstChild.innerText;
+    const products = this.recoverLocalStorage();
+    const inCart = products.some((product) => product.title.includes(element));
+
+    if (inCart) {
+      const index = products.findIndex((e) => e.title.includes(element));
+      products[index].quantity += 1;
+      // this.setState({ quantity: products[index].quantity });
+
+      localStorage.setItem('savedItems', JSON.stringify(products));
+    }
+  };
+
+  // decrementProduct = () =>{
+
+  // }
+
   render() {
     const products = this.recoverLocalStorage();
+    // const { quantity } = this.state;
 
     return (
       <div>
@@ -26,6 +49,20 @@ export default class ShoppingCart extends Component {
                   <p data-testid="shopping-cart-product-quantity">
                     { product.quantity }
                   </p>
+                  <button
+                    type="button"
+                    onClick={ this.incrementProduct }
+                    data-testid="product-increase-quantity"
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    // onClick={ this.decrementProduct }
+                    data-testid="product-decrease-quantity"
+                  >
+                    -
+                  </button>
                 </div>
               ))
             )
