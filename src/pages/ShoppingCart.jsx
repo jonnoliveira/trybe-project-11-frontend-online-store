53dc97e860b0f88cae245fe4b8998268a8e49c86
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-11-11 17:32:14 -0300

    Requisito 8 - completo

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class ShoppingCart extends Component {
+  recoverLocalStorage = () => {
+    const products = JSON.parse(localStorage.getItem('savedItems'));
+    return products;
+  };
+
   render() {
+    const products = this.recoverLocalStorage();
+
     return (
       <div data-testid="shopping-cart-empty-message">
-        <p>Seu carrinho está vazio</p>
+        {
+          products.map((product) => (
+            <div key={ product.title }>
+              <p data-testid="shopping-cart-product-name">
+                {product.title }
+              </p>
+              <p>
+                { product.price }
+              </p>
+              <p data-testid="shopping-cart-product-quantity">
+                { product.quantity }
+              </p>
+            </div>
+          ))
+        }
       </div>
     );
   }
